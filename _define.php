0320cf4126ab910085ea30cb5cc178bb951a8e92
franck <carnet.franck.paul@gmail.com>
2021-10-23 12:43:37 +0200

    Add alternative repository

--- _define.php
+++ _define.php
@@ -10,19 +10,23 @@
  * @copyright Franck Paul carnet.franck.paul@gmail.com
  * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
  */
-
-if (!defined('DC_RC_PATH')) {return;}
+if (!defined('DC_RC_PATH')) {
+    return;
+}
 
 $this->registerModule(
-    "Discrete category",                              // Name
-    "Exclude a category from Home and RSS/Atom feed", // Description
-    "Franck Paul",                                    // Author
+    'Discrete category',                              // Name
+    'Exclude a category from Home and RSS/Atom feed', // Description
+    'Franck Paul',                                    // Author
     '0.6',                                            // Version
     [
         'requires'    => [['core', '2.16']], // Dependencies
         'permissions' => 'admin',            // Permissions
         'type'        => 'plugin',           // Type
-        'settings'    => [                   // Settings
-        ]
+        'settings'    => [],
+
+        'details'    => 'https://open-time.net/?q=discreteCat',       // Details URL
+        'support'    => 'https://github.com/franck-paul/discreteCat', // Support URL
+        'repository' => 'https://raw.githubusercontent.com/franck-paul/discreteCat/main/dcstore.xml'
     ]
 );
