4a904f768f05a85d8893891b8addee7d25779857
franck <carnet.franck.paul@gmail.com>
2023-05-24 12:39:56 +0200

    l10n

--- locales/fr/main.lang.php
+++ locales/fr/main.lang.php
@@ -16,5 +16,5 @@ L10n::$locales['Exclude a category from Home and RSS/Atom feed'] = 'Exclut une c
 L10n::$locales['No category yet.'] = 'Aucune catégorie pour l\'instant.';
 L10n::$locales['This category will be excluded from home and it\'s RSS/Atom feeds only.'] = 'Cette catégorie sera exclue de la page d\'accueil et de ses flux RSS/Atom seulement.';
 L10n::$locales['Activate discrete categorie on this blog'] = 'Activer la catégorie discrète sur ce blog';
-L10n::$locales['Save'] = '';
+L10n::$locales['Save'] = 'Enregistrer';
 L10n::$locales['List of categories (non discrete)'] = 'Liste des catégories (non discrètes)';
