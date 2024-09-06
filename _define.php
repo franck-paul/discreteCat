66224938e14341313c2962e500697923a9fd5d4c
franck <carnet.franck.paul@gmail.com>
2024-09-06 08:31:16 +0200

    Code review

--- _define.php
+++ _define.php
@@ -23,6 +23,6 @@ $this->registerModule(
 
         'details'    => 'https://open-time.net/?q=discreteCat',
         'support'    => 'https://github.com/franck-paul/discreteCat',
-        'repository' => 'https://raw.githubusercontent.com/franck-paul/discreteCat/master/dcstore.xml',
+        'repository' => 'https://raw.githubusercontent.com/franck-paul/discreteCat/main/dcstore.xml',
     ]
 );
