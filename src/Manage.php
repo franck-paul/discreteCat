7c620c7ee448977e14184a1358146bde016cbe26
franck <carnet.franck.paul@gmail.com>
2025-09-22 10:12:41 +0200

    Use backend services rather than static calls

--- src/Manage.php
+++ src/Manage.php
@@ -16,8 +16,6 @@ declare(strict_types=1);
 namespace Dotclear\Plugin\discreteCat;
 
 use Dotclear\App;
-use Dotclear\Core\Backend\Notices;
-use Dotclear\Core\Backend\Page;
 use Dotclear\Helper\Html\Form\Checkbox;
 use Dotclear\Helper\Html\Form\Form;
 use Dotclear\Helper\Html\Form\Label;
@@ -65,7 +63,7 @@ class Manage
 
                 App::blog()->triggerBlog();
 
-                Notices::addSuccessNotice(__('Settings have been successfully updated.'));
+                App::backend()->notices()->addSuccessNotice(__('Settings have been successfully updated.'));
                 My::redirect();
             } catch (Exception $e) {
                 App::error()->add($e->getMessage());
@@ -99,15 +97,15 @@ class Manage
             // Ignore exceptions
         }
 
-        Page::openModule(My::name());
+        App::backend()->page()->openModule(My::name());
 
-        echo Page::breadcrumb(
+        echo App::backend()->page()->breadcrumb(
             [
                 Html::escapeHTML(App::blog()->name()) => '',
                 __('Discrete category')               => '',
             ]
         );
-        echo Notices::getNotices();
+        echo App::backend()->notices()->getNotices();
 
         // Prepare form fields
         $rs = App::blog()->getCategories(['post_type' => 'post']);
@@ -150,6 +148,6 @@ class Manage
             ])
         ->render();
 
-        Page::closeModule();
+        App::backend()->page()->closeModule();
     }
 }
