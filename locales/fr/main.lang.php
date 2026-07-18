28e70b51aff7468b844d53956d1fccae7e677870
franck <contact@open-time.net>
2026-07-18 18:44:11 +0200

    l10n

--- locales/fr/main.lang.php
+++ locales/fr/main.lang.php
@@ -12,10 +12,12 @@ declare(strict_types=1);
 
 use Dotclear\Helper\L10n;
 
-L10n::$locales['Discrete category'] = 'Catégorie discrète';
-L10n::$locales['Exclude a category from Home and RSS/Atom feed'] = 'Exclut une catégorie de la page d\'accueil et des flux RSS/Atom';
+L10n::$locales['Discrete categories'] = 'Catégories discrètes';
+L10n::$locales['Exclude categories from Home and RSS/Atom feed'] = 'Exclure certaines catégories de la page d\'accueil et du flux RSS/Atom';
 L10n::$locales['No category yet.'] = 'Aucune catégorie pour l\'instant.';
-L10n::$locales['This category will be excluded from home and it\'s RSS/Atom feeds only.'] = 'Cette catégorie sera exclue de la page d\'accueil et de ses flux RSS/Atom seulement.';
-L10n::$locales['Activate discrete categorie on this blog'] = 'Activer la catégorie discrète sur ce blog';
+L10n::$locales['Category'] = 'Catégorie';
+L10n::$locales['Number of posts'] = 'Nombre d\'entrées';
+L10n::$locales['Activate discrete categories on this blog'] = 'Activer les catégories discrètes sur ce blog';
+L10n::$locales['The following selected categories will be excluded from home and it\'s RSS/Atom feeds only.'] = 'Les catégories sélectionnées ci-dessous seront exclues uniquement de la page d\'accueil et de son flux RSS/Atom.';
 L10n::$locales['Save'] = 'Enregistrer';
 L10n::$locales['List of categories (non discrete)'] = 'Liste des catégories (non discrètes)';
